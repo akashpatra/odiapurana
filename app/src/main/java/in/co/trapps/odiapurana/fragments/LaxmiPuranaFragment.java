4a32535c5319574e94892895113573243a91b63f
Akash Patra <patraakash9@gmail.com>
2018-01-28 13:28:25 +0530

    Bug: Odia Font Issue in Lollipop  Fixes akashpatra/odiapurana#8

--- app/src/main/java/in/co/trapps/odiapurana/fragments/LaxmiPuranaFragment.java
+++ app/src/main/java/in/co/trapps/odiapurana/fragments/LaxmiPuranaFragment.java
@@ -1,12 +1,16 @@
 package in.co.trapps.odiapurana.fragments;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import in.co.trapps.odiapurana.R;
 import in.co.trapps.odiapurana.constants.Config;
 import in.co.trapps.odiapurana.logger.Logger;
@@ -19,6 +23,9 @@ public class LaxmiPuranaFragment extends Fragment {
 
     // For Logging
     private final LoggerEnable CLASS_NAME = LoggerEnable.LaxmiPuranaFragment;
+
+    @BindView(R.id.tv_lyrics)
+    public TextView tvLyrics;
 
     public LaxmiPuranaFragment() {
         // Required empty public constructor
@@ -36,13 +43,20 @@ public class LaxmiPuranaFragment extends Fragment {
         Logger.logD(Config.TAG, CLASS_NAME, " >> onCreateView");
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_laxmi_purana, container, false);
+        View view = inflater.inflate(R.layout.fragment_laxmi_purana, container, false);
+        ButterKnife.bind(this, view);
+
+        return view;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         Logger.logD(Config.TAG, CLASS_NAME, " >> onViewCreated");
+
+        // Custom Font for Lyrics
+        Typeface droidFont = Typeface.createFromAsset(getActivity().getAssets(), Config.FONT_REGIONAL);
+        tvLyrics.setTypeface(droidFont);
     }
 
     @Override
